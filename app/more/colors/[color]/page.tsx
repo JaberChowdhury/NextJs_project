f58fe6337271486a940c4117b5bbc6e1fcd8c0ac
black <jaberhc2002@gmail.com>
2024-03-19 03:24:04 +0600

    fix : type problem

--- app/more/colors/[color]/page.tsx
+++ app/more/colors/[color]/page.tsx
@@ -1,17 +1,20 @@
 import utils from "@/lib/utils";
 
 interface propsType {
-  params: string;
+  params: {
+    color : string
+    }
 }
 
 const Tools = ({ params }: propsType) => {
+  
   const utility = new utils(`#${params.color}`);
-
+  
   return (
     <div>
       <span className="loading loading-dots loading-lg"></span>
       <div>{params.color}</div>
-      {JSON.stringify(utility.getColor())}
+      <div className="p-2">{JSON.stringify(utility.getColor())}</div>
     </div>
   );
 };
