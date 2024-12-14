cb05734f6e0d5f1d6fa60523c7029abd402383a1
black <jaberhc2002@gmail.com>
2024-12-14 15:10:44 +0600

    update : react 19 and next 15

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Provider from "@/components/provider";
 
-import Navbar from "@/components/Navbar";
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-const inter = Inter({ subsets: ["latin"] });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -19,11 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Provider>
-          <Navbar />
-          <main className="px-4">{children}</main>
-        </Provider>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
       </body>
     </html>
   );
