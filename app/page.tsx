dd9c9c2ababd9616cd9f10dbb17a9f3702d84a66
Black Light <78747748+JaberChowdhury@users.noreply.github.com>
2025-01-17 18:58:31 +0000

    add: implement product table with mock data and remove old page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,49 @@
 import React from "react";
-import { faker } from "@faker-js/faker";
-import { Typography } from "@mui/material";
-const page = () => {
-  const product_name = faker.commerce.product();
-  const product_price = faker.commerce.price({
-    min: 100,
-    max: 3000,
-  });
-  return (
-    <div>
-      page
-      <Typography><Typography variant="h1">name</Typography>{product_name}</Typography>
-      <Typography><Typography variant="h1">price</Typography>{product_price}</Typography>
-    </div>
-  );
-};
+import product, { type PRODUCT } from "@/data/product";
 
-export default page;
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+export default function BasicTable() {
+  return (
+    <TableContainer sx={{ maxHeight: "100vh" }} component={Paper}>
+      <Table stickyHeader sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Serial</TableCell>
+            <TableCell align="right">ID</TableCell>
+            <TableCell align="right">Owner</TableCell>
+            <TableCell align="right">Name</TableCell>
+            <TableCell align="right">Price</TableCell>
+            <TableCell align="right">Checked</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {product.map((row: PRODUCT, dataid) => (
+            <TableRow
+              key={row.id}
+              sx={{
+                border: "0px solid transparent",
+                "&:last-child td, &:last-child th": { border: 0 },
+              }}
+            >
+              <TableCell component="th" scope="row">
+                {dataid + 1}
+              </TableCell>
+              <TableCell align="right">{row.id}</TableCell>
+              <TableCell align="right">{row.owner}</TableCell>
+              <TableCell align="right">{row.name}</TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+              <TableCell align="right">{row.checked}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ data/product.ts
@@ -0,0 +1,25 @@
+import { faker } from "@faker-js/faker";
+
+export interface PRODUCT {
+  name: string;
+  id: string;
+  price: string;
+  owner: string;
+  checked: boolean;
+}
+
+const product: PRODUCT[] = [];
+const limit = 1000;
+
+for (let i = 0; i < limit; i++) {
+  const data = {
+    name: faker.commerce.product(),
+    price: faker.commerce.price({ min: 100, max: 500 }),
+    id: crypto.randomUUID(),
+    owner: faker.person.firstName(),
+    checked: false,
+  };
+  product.push(data);
+}
+
+export default product;
